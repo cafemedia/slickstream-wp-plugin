dcfb10de0bc058add4835e597b667250875e27dd
Eric Perez <eperez@raptive.com>
2024-11-11 14:52:43 -0700

    [SLICK-1691] removed unnecessary `update URI` from metadata

--- svn/trunk/SlickEngagement.php
+++ svn/trunk/SlickEngagement.php
@@ -10,7 +10,6 @@
  * Author URI:        https://slickstream.com
  * License:           GPL v3 or later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
- * Update URI:        https://example.com/my-plugin/
  * Text Domain:       slick-engagement
  */
 
